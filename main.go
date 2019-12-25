7d78ed6dd6c9e23ea51f8bc97fc5ef6a1f09f504
Andrew <andrew33673367@cht.com.tw>
2019-12-25 17:30:45 +0800

    update 1225 v9

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ import (
 )
 
 //wfwaf
-//awfawawawawawawawawawawawawawafawfawfawffwawfawfwfwfwfwafw
+//awfawawawawawawawawawawawawawafawfawfawffwawfawfwfwfwfwafwafawf
 func loopfunc() {
 	for {
 		time.Sleep(time.Duration(500) * time.Millisecond)
